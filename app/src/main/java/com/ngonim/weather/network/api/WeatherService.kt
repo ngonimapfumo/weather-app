ebe9f951e2af0074d353274849ae81f5dea132df
Ngoni <ngonimapfumo@hotmail.com>
2025-07-23 19:24:03 +0200

    add base files

--- app/src/main/java/com/ngonim/weather/network/api/WeatherService.kt
+++ app/src/main/java/com/ngonim/weather/network/api/WeatherService.kt
@@ -1,6 +1,14 @@
 package com.ngonim.weather.network.api
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface WeatherService {
 
+    @GET("/v1/current.json")
+    suspend fun getWeather(
+        @Query("key") apiKey: String,
+        @Query("q") city: String
+    )
 
 }
