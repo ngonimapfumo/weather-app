d641f22bb5fce104ce6a2f7da321a1b63b675bc2
Ngoni <ngonimapfumo@hotmail.com>
2025-08-27 18:08:43 +0200

    Refactor: Improve WeatherAlertDialog layout
    This commit updates the `WeatherAlertDialog` composable to enhance its layout and visual appearance.
    
    Key changes:
    - Modified `WeatherAlertDialog.kt`:
        - Changed the "OK" `TextButton` to span the full width of the dialog.
        - Added padding around the "OK" `TextButton`.

--- app/src/main/java/com/ngonim/weather/presentation/uidialogs/WeatherAlertDialog.kt
+++ app/src/main/java/com/ngonim/weather/presentation/uidialogs/WeatherAlertDialog.kt
@@ -2,13 +2,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ngonim.weather.util.GenUtil.formatDate
@@ -26,7 +29,9 @@ fun WeatherAlertDialog(
     AlertDialog(
         onDismissRequest = { onDismiss() },
         confirmButton = {
-            TextButton(onClick = onDismiss) {
+            TextButton(onClick = onDismiss, modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)) {
                 Text("OK")
             }
         },
